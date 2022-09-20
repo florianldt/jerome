6a290f9af14e28271a460c07d433bc47bf03b0b3
Florian Ludot <florian.ludot@gmail.com>
2022-09-20 12:13:51 +0900

    feat: improve logging

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import { Command, Option } from 'commander';
+import { join } from 'path';
 
 import { papagoLocals, parseFile, translate, writeFile } from './lib';
 import version from './version';
@@ -38,14 +39,37 @@ program.parse();
 
 const { input, source, target } = program.opts();
 
+/* eslint-disable no-console */
 async function run() {
     try {
+        console.log('       _                               ');
+        console.log('      | |                              ');
+        console.log('      | | ___ _ __ ___  _ __ ___   ___ ');
+        console.log("  _   | |/ _ \\ '__/ _ \\| '_ ` _ \\ / _ \\");
+        console.log(' | |__| |  __/ | | (_) | | | | | |  __/');
+        console.log(`  \\____/ \\___|_|  \\___/|_| |_| |_|\\___| v${version}`);
+
+        console.log();
+        console.log(`${chalk.bold('Input:')} ${input}`);
+        console.log(`${chalk.bold('Source:')} ${source}`);
+        console.log(`${chalk.bold('Target:')} ${target}`);
+        console.log();
+
         const keyValues = await parseFile(input);
+
         const translations = await translate(keyValues, source, target);
-        writeFile(target, keyValues, translations, input);
+        const filePath = writeFile(target, keyValues, translations, input);
+
+        console.log(
+            chalk.green(
+                `Successfully translated from '${source}' to '${target}' at ${join(
+                    __dirname,
+                    filePath,
+                )}`,
+            ),
+        );
     } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log(chalk.red(e));
+        console.log(chalk.bold.red(e));
     }
 }
 
